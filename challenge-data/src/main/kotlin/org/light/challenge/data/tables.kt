f50303f176257a130bc845713ef937a6338a8e88
lsimonar <lluis.simon.92@gmail.com>
2023-02-07 22:02:30 +0100

    Renamed tables

--- challenge-data/src/main/kotlin/org/light/challenge/data/tables.kt
+++ challenge-data/src/main/kotlin/org/light/challenge/data/tables.kt
@@ -3,14 +3,14 @@ package org.light.challenge.data
 // TODO: placeholder - DB tables here
 import org.jetbrains.exposed.sql.*
 
-object Invoices : Table() {
+object InvoicesTable : Table() {
     val id = integer("id").autoIncrement().primaryKey()
     val amount = double("amount")
     val department = text("department")
     val requiresManagerApproval = bool("requires_manager_approval")
 }
 
-object Employees : Table() {
+object EmployeesTable : Table() {
     val username = text("username").uniqueIndex().primaryKey()
     val name = text("name")
     val role = text("role")
@@ -18,15 +18,15 @@ object Employees : Table() {
     val slack = text("slack")
 }
 
-object Rules : Table() {
+object RulesTable : Table() {
     val id = integer("id").autoIncrement().primaryKey()
     val department = (text("department"))
     val minAmount = double("min_amount")
     val maxAmount = double("max_amount")
     val contactMethod = text("contact_method")
-    val employeeUsername = (text("employee_username") references Employees.username)
+    val employeeUsername = (text("employee_username") references EmployeesTable.username)
 }
 
-object Workflow : Table() {
-    val ruleIds = (integer("rule_id") references Rules.id)
+object WorkflowTable : Table() {
+    val ruleIds = (integer("rule_id") references RulesTable.id)
 }
