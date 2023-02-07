69f8349dbb5ac74bd39fc3eca7276d486f5189cc
lsimonar <lluis.simon.92@gmail.com>
2023-02-07 21:58:00 +0100

    created typehelper class

---
+++ challenge-core/src/main/kotlin/org/light/challenge/logic/core/TypeHelper.kt
@@ -0,0 +1,44 @@
+package org.light.challenge.logic.core
+
+class TypeHelper {
+    enum class Department {
+        MARKETING,
+        FINANCE
+    }
+
+    enum class ContactMethod {
+        EMAIL,
+        SLACK
+    }
+
+    data class Invoice(
+        val id: Int,
+        val amount: Double,
+        val department: Department,
+        val requiresManagerApproval: Boolean
+    )
+
+    data class Employee(
+        val username: String,
+        val name: String,
+        val role: String,
+        val email: String,
+        val slack: String
+    )
+
+    data class Rule(
+        val department: Department,
+        val amountRange: Pair<Double, Double>,
+        val employeeUsername: String,
+        val contactMethod: ContactMethod
+    )
+
+    fun toDepartment(input: String): Department {
+        return Department.valueOf(input.uppercase())
+    }
+
+    fun toContactMethod(input: String): ContactMethod {
+        return ContactMethod.valueOf(input.uppercase())
+    }
+
+}
