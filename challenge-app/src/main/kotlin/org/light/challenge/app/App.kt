89667325e209ce64a985879017ae159c88d1563f
lsimonar <lluis.simon.92@gmail.com>
2023-02-07 22:21:40 +0100

    updated table names and created some mess

--- challenge-app/src/main/kotlin/org/light/challenge/app/App.kt
+++ challenge-app/src/main/kotlin/org/light/challenge/app/App.kt
@@ -6,6 +6,8 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.light.challenge.data.*
+import org.light.challenge.logic.core.TypeHelper
+import org.light.challenge.logic.core.TypeHelper.*
 
 fun main() {
 
@@ -14,28 +16,51 @@ fun main() {
 
     // TODO: placeholder - start the program here
     val workflowService = WorkflowService()
-    transaction(db) {
-        SchemaUtils.create(Employees, Rules, Workflow)
-    }
+    val typeHelper = TypeHelper()
 
-    transaction(db) {
-    workflowService.addRule(
-        WorkflowService.Rule(
-            WorkflowService.Department.Marketing,
-            Pair(5000.0, 10000.0),
-            "marketing_manager",
-            WorkflowService.ContactMethod.Email,
-    ))}
+    println("Enter a rule")
+    println("Department: ")
+    val department = readLine()
+    println("Minimum amount: ")
+    val minAmount = readln().toDouble()
+    println("Maximum amount: ")
+    val maxAmount = readln().toDouble()
+    println("Contact method: ")
+    val contactMethod = readln()
+    println("Employee username: ")
+    val employeeUsername = readln()
 
+    // Manually inputting a rule and rule id inside workflow
     transaction(db) {
-    val rules = Rules.selectAll().toList()
-    for (rule in rules) {
-        println("ID: ${rule[Rules.id]}")
-        println("Department: ${rule[Rules.department]}")
-        println("Min Amount: ${rule[Rules.minAmount]}")
-        println("Max Amount: ${rule[Rules.maxAmount]}")
-        println("Contact Method: ${rule[Rules.contactMethod]}")
-        println("Employee username: ${rule[Rules.employeeUsername]}")
+
+        SchemaUtils.create(EmployeesTable, RulesTable, WorkflowTable)
+
+        val ruleId = workflowService.addRule(
+            Rule(
+                typeHelper.toDepartment(department!!),
+                Pair(minAmount, maxAmount),
+                employeeUsername,
+                typeHelper.toContactMethod(contactMethod),
+            )
+        )
+
+        workflowService.addRuleIdToWorkflow(ruleId)
+
+        val rules = RulesTable.selectAll().toList()
+        for (rule in rules) {
+            println("ID: ${rule[RulesTable.id]}")
+            println("Department: ${rule[RulesTable.department]}")
+            println("Min Amount: ${rule[RulesTable.minAmount]}")
+            println("Max Amount: ${rule[RulesTable.maxAmount]}")
+            println("Contact Method: ${rule[RulesTable.contactMethod]}")
+            println("Employee username: ${rule[RulesTable.employeeUsername]}")
+            println()
+        }
+
+        val workFlow = WorkflowTable.selectAll().toList()
         println()
-    }}
+        for (ruleId in workFlow) {
+            println("RuleId: ${ruleId[WorkflowTable.ruleIds]}")
+        }
+    }
 }
