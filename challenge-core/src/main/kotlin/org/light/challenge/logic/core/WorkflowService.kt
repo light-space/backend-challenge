d2be3005a497c7c4127669110100260359d3063f
lsimonar <lluis.simon.92@gmail.com>
2023-02-07 01:30:15 +0100

    defined WorkflowService data classes

--- challenge-core/src/main/kotlin/org/light/challenge/logic/core/WorkflowService.kt
+++ challenge-core/src/main/kotlin/org/light/challenge/logic/core/WorkflowService.kt
@@ -2,4 +2,36 @@ package org.light.challenge.logic.core
 
 class WorkflowService {
     // TODO: placeholder - workflow logic here
+    enum class Department {
+        Marketing,
+        Finance
+    }
+
+    enum class ContactMethod {
+        Email,
+        Slack
+    }
+
+    data class Invoice(
+        val id: Int,
+        val amount: Double,
+        val department: Department,
+        val requiresManagerApproval: Boolean
+    )
+
+    data class Employee(
+        val username: String,
+        val name: String,
+        val role: String,
+        val email: String,
+        val slack: String
+    )
+
+    data class Rule(
+        val department: Department,
+        val amountRange: Pair<Double, Double>,
+        val employeeUsername: String,
+        val contactMethod: ContactMethod
+    )
+
 }
