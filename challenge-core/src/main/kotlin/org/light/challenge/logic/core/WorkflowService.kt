9cf9f4d125a1538312faab61c884267b24789300
lsimonar <lluis.simon.92@gmail.com>
2023-02-08 01:10:25 +0100

    implemented processInvoice function

--- challenge-core/src/main/kotlin/org/light/challenge/logic/core/WorkflowService.kt
+++ challenge-core/src/main/kotlin/org/light/challenge/logic/core/WorkflowService.kt
@@ -1,58 +1,62 @@
 package org.light.challenge.logic.core
 
 import org.light.challenge.data.*
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.light.challenge.logic.core.TypeHelper.*
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+
 class WorkflowService {
     // TODO: placeholder - workflow logic here
-    enum class Department {
-        Marketing,
-        Finance
-    }
-
-    enum class ContactMethod {
-        Email,
-        Slack
-    }
-
-    data class Invoice(
-        val id: Int,
-        val amount: Double,
-        val department: Department,
-        val requiresManagerApproval: Boolean
-    )
-
-    data class Employee(
-        val username: String,
-        val name: String,
-        val role: String,
-        val email: String,
-        val slack: String
-    )
-
-    data class Rule(
-        val department: Department,
-        val amountRange: Pair<Double, Double>,
-        val employeeUsername: String,
-        val contactMethod: ContactMethod
-    )
 
      fun addRule(rule: Rule): Int {
-        val newRuleId = Rules.insert {
+        val newRuleId = RulesTable.insert {
             it[department] = rule.department.name
             it[minAmount] = rule.amountRange.first
             it[maxAmount] = rule.amountRange.second
+            it[requiresManagerApproval] = rule.requiresManagerApproval
             it[contactMethod] = rule.contactMethod.name
             it[employeeUsername] = rule.employeeUsername
-        } get Rules.id
+        } get RulesTable.id
 
         return newRuleId
      }
 
     fun addRuleIdToWorkflow(ruleId: Int) {
-        Workflow.insert {
+        WorkflowTable.insert {
             it[ruleIds] = ruleId
         }
     }
 
+    fun addInvoice(invoice: Invoice) {
+        InvoicesTable.insert {
+            it[id] = invoice.id
+            it[amount] = invoice.amount
+            it[department] = invoice.department.name
+            it[requiresManagerApproval] = invoice.requiresManagerApproval
+        }
+    }
+
+    fun processInvoice(invoice: Invoice): String {
+        val workflow = WorkflowTable.selectAll().toList()
+        val ruleIds = workflow.map { it[WorkflowTable.ruleIds] }
+
+        for (ruleId in ruleIds) {
+            val rule = RulesTable.select { RulesTable.id eq ruleId }.first()
+            val department = Department.valueOf(rule[RulesTable.department])
+            val minAmount = rule[RulesTable.minAmount]
+            val maxAmount = rule[RulesTable.maxAmount]
+            val requiresManagementApproval = rule[RulesTable.requiresManagerApproval]
+            val contactMethod = ContactMethod.valueOf(rule[RulesTable.contactMethod])
+            val employeeUsername = rule[RulesTable.employeeUsername]
+
+            if (invoice.department === department &&
+                invoice.amount in minAmount..maxAmount &&
+                invoice.requiresManagerApproval == requiresManagementApproval
+            ) {
+                return "Send a message via $contactMethod to $employeeUsername"
+            }
+        }
+        return "None of the rules passed."
+    }
 }
