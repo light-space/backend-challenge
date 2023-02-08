290813dbaf96c35375558d8cf42efa99b1680cd6
lsimonar <lluis.simon.92@gmail.com>
2023-02-08 04:14:40 +0100

    implemented addrule command

--- challenge-app/src/main/kotlin/org/light/challenge/app/App.kt
+++ challenge-app/src/main/kotlin/org/light/challenge/app/App.kt
@@ -25,13 +25,13 @@ fun main(args: Array<String>) {
     when (args[0]) {
           "--submit-invoice" -> {
             if(args.size == 4){
-                val newInvoice = Invoice(
-                    amount = args[1].toDouble(),
-                    department = typeHelper.toDepartment(args[2]),
-                    requiresManagerApproval = args[3].toBoolean()
-                )
                 transaction(db) {
-                    val sendInvoiceTo = workflowService.processInvoice(newInvoice)
+                    val sendInvoiceTo = workflowService.processInvoice(
+                        Invoice(
+                            amount = args[1].toDouble(),
+                            department = typeHelper.toDepartment(args[2]),
+                            requiresManagerApproval = args[3].toBoolean())
+                    )
                     println(sendInvoiceTo)
                 }
             } else {
@@ -45,43 +45,41 @@ fun main(args: Array<String>) {
             }
         }
         "--add-rule-to-workflow" -> {
-            if(args.size == 6) {
-                transaction(db) {
+            while(true) {
+                println("Enter the next rule in the workflow. Press enter if you wish to skip a constraint.")
+                println("Minimum amount: ")
+                val minAmount = readln().toDouble()
+                println("Maximum amount: ")
+                val maxAmount = readln().toDouble()
+                println("Department: ")
+                val department = typeHelper.toDepartment(readln())
+                println("Requires Manager Approval [true / false]: ")
+                val requiresManagerApproval = readln().toBoolean()
+                println("Employee username: ")
+                val employeeUsername = readln()
+                println("Contact method: ")
+                val contactMethod = typeHelper.toContactMethod(readln())
 
+                transaction(db) {
                     val ruleId = workflowService.addRule(
                         Rule(
-                            amountRange = Pair(args[0].toDouble(), args[1].toDouble()),
-                            department = typeHelper.toDepartment(args[2]),
-                            requiresManagerApproval = args[3].toBoolean(),
-                            employeeUsername = args[4],
-                            contactMethod = typeHelper.toContactMethod(args[5])
-                    ))
+                            amountRange = Pair(minAmount, maxAmount),
+                            department = department,
+                            requiresManagerApproval = requiresManagerApproval,
+                            employeeUsername = employeeUsername,
+                            contactMethod = contactMethod
+                        )
+                    )
                     workflowService.addRuleIdToWorkflow(ruleId)
                 }
-            } else {
-                println("""Usage: ./gradlew run --args="--add-rule-to-workflow <min_amount> <max_amount> <department [Marketing/Finance]>
-                    |      <require_manager_approval> <receiver_username> <contact_method [email/slack]>"""")
-                return
+                println("Rule successfully included in the workflow, do you wish to enter another rule? [y/n]")
+                val wishToContinue = readln()
+                if(wishToContinue == "y") continue else break;
             }
         }
     }
 
-    /*
-    println("Enter a rule")
-    println("Department: ")
-    val department = readLine()
-    println("Minimum amount: ")
-    val minAmount = readln().toDouble()
-    println("Maximum amount: ")
-    val maxAmount = readln().toDouble()
-    println("Requires Manager Approval [true / false]: ")
-    val requiresManagerApproval = readln().toBoolean()
-    println("Contact method: ")
-    val contactMethod = readln()
-    println("Employee username: ")
-    val employeeUsername = readln()
-
-*/      transaction(db) {
+      transaction(db) {
         val rules = RulesTable.selectAll().toList()
         for (rule in rules) {
             println("ID: ${rule[RulesTable.id]}")
